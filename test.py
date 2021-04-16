b9a7c88104d8c0d0ad45cfc0796ffbbc67a0d308
axer1001 <4DaniilRubin@protonmail.com>
2021-04-16 13:53:19 +0300

    Writing

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print(1)
+from sympy.abc import a, x, u
+from sympy import Integral, cos, sin, tan
+from sympy import trigsimp
+i = Integral((4 - x ** 2)**(1/2), x)
+#i = Integral(1/(2 * sin(x) + 3 * cos(x) - 1), x)
+i = i.trig_transform(x, 2 * sin(a))
+print(i.trigsimp())
