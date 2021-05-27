b8a94ca6e4a7d9b97204e90a76b1b3cdd5fb1def
axer1001 <4DaniilRubin@protonmail.com>
2021-05-28 00:21:48 +0300

    Removed timer

--- trig_transform.py
+++ trig_transform.py
@@ -6,35 +6,64 @@ from sympy import Abs
 x = Symbol('x')
 t = Symbol('t', real=True)
 
-i1 = Integral(sqrt(4-x**2), (x, -2 , -1))
-i1 = i1.trig_transform(x, 2 * sin(t))
+i1 = Integral(sqrt(4 - x ** 2), (x, -2, -1))
+transformed = i1.trig_transform(x, 2 * sin(t), debug=True)
 
-i2 = Integral(sqrt(x**2-4), (x, 3, 4))
-i2 = i2.trig_transform(x, 2/sin(t))
+print(transformed)
 
-i3 = Integral(sqrt(x**2 + 4), (x, -3, 1))
-i3 = i3.trig_transform(x, 2 * tan(t))
+i2 = Integral(sqrt(x ** 2 - 4), (x, 3, 4))
+i2 = i2.trig_transform(x, 2 / sin(t), debug=True)
+
+i3 = Integral(sqrt(x ** 2 +  x**3 + 4), (x, -3, 1))
+i3 = i3.trig_transform(x, 2 * tan(t), debug=True)
 
 # sin(x) = 5 exception!
-#i1 = i1.xreplace({Abs(cos(t)) : -cos(t)})
-print(i1.doit(), i2.doit(), i3.doit(), sep='\n')
+# i1 = i1.xreplace({Abs(cos(t)) : -cos(t)})
+#print(i1.doit(), i2.doit(), i3.doit(), sep='\n')
 
 
+def trig_transform(self, x, u, debug=False):
+    """
+    Perform the trig_transform with simplification
 
-def trig_transform(self, x, u, debug=False, t=0):
-    
-    def empty_handler(signal, f):
-        return
+    Supported functions:
+        x = a * sin(t),
+        x = a * cos(t),
+        x = a / sin(t)
+        x = a * tg(t)
 
-    if t:
-        import signal
+    Examples
+    ========
 
-        def signal_handler(signal, f):
-            raise Exception("Time Out!")
-    
-        signal.signal(signal.SIGALRM, signal_handler)
-        signal.alarm(t)
+    >>> from sympy import Integral
+    >>> from sympy import sqrt
+    >>> from sympy.abc import x, t
+    >>> p = Integral(sqrt(4 - x ** 2), (x, -2, -1))
+    >>> p.trig_transform(x, 2 * sin(t))
+    Integral(4*cos(t)**2, (t, -pi/2, -pi/6))
 
+    Errors
+    ======
+
+    >>> from sympy import Integral
+    >>> from sympy import sqrt
+    >>> from sympy.abc import x, t
+    >>> p = Integral(sqrt(4 - x ** 2), (x, -6, 7))
+    >>> p.trig_transform(x, 2 * sin(t))
+
+    Value error
+    \not \exists t : 2 * sin(t) = -6
+
+    Same value error for 2 / sin(t)
+
+    See Also
+    ========
+
+    sympy.integrals.trigonometry.trigintegrate
+    sympy.integrals.heurisch.heurisch
+    sympy.integrals.rationaltools.ratint
+    as_sum : Approximate the integral using a sum
+    """
     from sympy.solvers.solvers import solve, posify
     from sympy import cos, sin, tan, cot, sqrt
 
@@ -42,7 +71,6 @@ def trig_transform(self, x, u, debug=False, t=0):
 
     xfree = x.free_symbols.intersection(self.variables)
     if len(xfree) > 1:
-        signal.signal(signal.SIGALRM, empty_handler)
         raise ValueError(
             'F(x) can only contain one of: %s' % self.variables)
     xvar = xfree.pop() if xfree else d
@@ -54,41 +82,27 @@ def trig_transform(self, x, u, debug=False, t=0):
     if isinstance(u, Expr):
         ufree = u.free_symbols
         if len(ufree) == 0:
-            signal.signal(signal.SIGALRM, empty_handler)
             raise ValueError(filldedent('''
-                f(u) cannot be a constant'''))
+                    f(u) cannot be a constant'''))
         if len(ufree) > 1:
-            signal.signal(signal.SIGALRM, empty_handler)
-            raise ValueError(filldedent('''
-                When f(u) has more than one free symbol, the one replacing x
-                must be identified: pass f(u) as (f(u), u)'''))
+            raise ValueError(filldedent('''More than 1 free symbol'''))
         uvar = ufree.pop()
     else:
         u, uvar = u
-        if uvar not in u.free_symbols:
-            signal.signal(signal.SIGALRM, empty_handler)
-            raise ValueError(filldedent('''
-                Expecting a tuple (expr, symbol) where symbol identified
-                a free symbol in expr, but symbol is not in expr's free
-                symbols.'''))
 
     if x.is_Symbol and u.is_Symbol:
-        signal.signal(signal.SIGALRM, empty_handler)
         return self.xreplace({x: u})
 
     if not x.is_Symbol and not u.is_Symbol:
-        signal.signal(signal.SIGALRM, empty_handler)
         raise ValueError('either x or u must be a symbol')
 
     if uvar == xvar:
-        signal.signal(signal.SIGALRM, empty_handler)
         return self.transform(x, (u.subs(uvar, d), d)).xreplace({d: uvar})
 
     if uvar in self.limits:
-        signal.signal(signal.SIGALRM, empty_handler)
         raise ValueError(filldedent('''
-            u must contain the same variable as in x
-            or a variable that is not already an integration variable'''))
+                u must contain the same variable as in x
+                or a variable that is not already an integration variable'''))
 
     A = u.args[0] if len(u.args) > 1 else 1
 
@@ -104,15 +118,14 @@ def trig_transform(self, x, u, debug=False, t=0):
     if debug:
         print("New df:", newDf)
 
-    if len(dfAndlimits) == 1: # indefinite integral
+    if len(dfAndlimits) == 1:  # indefinite integral
         newI = self.func(newF * diff(newDf))
-        
-        signal.signal(signal.SIGALRM, empty_handler)
+
         return newI.trigsimp().xreplace({
-            Abs(cos(uvar)) : cos(uvar),
-            Abs(sin(uvar)) : sin(uvar),
-            Abs(tan(uvar)) : tan(uvar),
-            Abs(cot(uvar)) : cot(uvar)
+            Abs(cos(uvar)): cos(uvar),
+            Abs(sin(uvar)): sin(uvar),
+            Abs(tan(uvar)): tan(uvar),
+            Abs(cot(uvar)): cot(uvar)
         }).trigsimp()
 
     # definite integral
@@ -122,24 +135,22 @@ def trig_transform(self, x, u, debug=False, t=0):
 
         # exception if wrong limits for sin
         if u.args[-1] == sin(uvar) and (abs(a / A) > 1 or abs(b / A) > 1):
-            signal.signal(signal.SIGALRM, empty_handler)
-            raise(ValueError("Wrong limits"))
-        
-        signal.signal(signal.SIGALRM, empty_handler)
+            raise ValueError("Wrong limits")
+
         return F.subs(xvar, a), F.subs(xvar, b)
 
     oldLimits = (self.limits[0][1], self.limits[0][2])
 
     a, b = __calc_limits(oldLimits[0], oldLimits[1])
     newI = self.func(newF * diff(newDf), (uvar, a, b))
-    
+
     if debug:
         print("New limits:", a, b)
 
-    signal.signal(signal.SIGALRM, empty_handler)
     return newI.trigsimp().xreplace({
-        Abs(cos(uvar)) : cos(uvar),
-        Abs(sin(uvar)) : sin(uvar),
-        Abs(tan(uvar)) : tan(uvar),
-        Abs(cot(uvar)) : cot(uvar)
+        Abs(cos(uvar)): cos(uvar),
+        Abs(sin(uvar)): sin(uvar),
+        Abs(tan(uvar)): tan(uvar),
+        Abs(cot(uvar)): cot(uvar)
     }).trigsimp()
+
